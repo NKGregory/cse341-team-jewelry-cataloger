38f5362ff47b0e31f4cb30beb8b9747c7c5edf4e
Darrin Fry <productionstagemanager@gmail.com>
2022-06-16 11:47:51 -0600

    swagger and index fixes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const routes = require('express').Router();
 routes.use('/', require('./swagger'));
 routes.use('/user', require('./user'));
 routes.use('/insurance', require('./insurance'));
-routes.use('/jewelry', require('./jewelery'));
+routes.use('/jewelry', require('./jewelry'));
 routes.use('/market', require('./market'));
 
 module.exports = routes;
---
+++ routes/swagger.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
+
+
+router.use('/api-docs', requiresAuth(), swaggerUi.serve);
+router.get('/api-docs', requiresAuth(), swaggerUi.setup(swaggerDocument));
+
+module.exports = router;
